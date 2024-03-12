487fa6d944efc0bdcaa27d7d0a6947061e34e608
suhaskolhe11 <suhaskolhe1111@gmail.com>
2024-03-12 07:12:39 +0530

    Affirmations app

--- compose-training-affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ compose-training-affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
@@ -57,7 +58,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@Preview
 @Composable
 fun AffirmationsApp() {
     AffirmationList(
